0896fa080ee576569c15553b9e1d4a026c3ff800
Evan Lewis <el616412@ohio.edu>
2022-11-17 20:44:14 +0900

    ToDo DB created

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,17 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   movies = Movie.create(
+  #   [
+  #     { name: "Star Wars" },
+  #     { name: "Lord of the Rings" }
+  #   ]
+  # )
 #   Character.create(name: "Luke", movie: movies.first)
+# tasks = Task.create(
+#   { name: "Get up"},
+#   { name: "Find pants"}
+# )
+
+task1 = Task.create(name: "Get Up")
+task2 = Task.create(name: "Find pants")
