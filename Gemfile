c1bfaac71e6693ba97ec3833acd42acbc990d2c6
Evan Lewis <el616412@ohio.edu>
2022-11-19 15:16:57 +0900

    made task controllers

--- Gemfile
+++ Gemfile
@@ -42,7 +42,6 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
-
 # Use Sass to process CSS
 # gem "sassc-rails"
 
@@ -71,5 +70,3 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
-
-gem "tailwindcss-rails", "~> 2.0"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,9 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create(
-  #   [
-  #     { name: "Star Wars" },
-  #     { name: "Lord of the Rings" }
-  #   ]
-  # )
-#   Character.create(name: "Luke", movie: movies.first)
-# tasks = Task.create(
-#   { name: "Get up"},
-#   { name: "Find pants"}
-# )
+puts "Creating tasks"
+Task.destroy.all
 
 task1 = Task.create(name: "Get Up")
 task2 = Task.create(name: "Find pants")
+
+puts "Tasks created"
